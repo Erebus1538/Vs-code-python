50b9beb260a5ed13373c87e737bc792a31025eb8
erebus1538 <erebus152@yahoo.com>
2023-11-22 10:50:32 -0700

    i also wanna add this shit

--- testy.py
+++ testy.py
@@ -1,3 +1,4 @@
 print("hello world")
 print("my name is not jamal")
 print("would be able to set up this shit")
+# i also added this massgae just to confirm that i have learned that i can use github by vs code
