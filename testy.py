3e8e7c53c7044e081d0bfab5e1573b3d3c782c96
erebus1538 <erebus152@yahoo.com>
2023-11-22 13:54:21 -0700

    yup

--- testy.py
+++ testy.py
@@ -2,3 +2,4 @@ print("hello world")
 print("my name is not jamal")
 print("would be able to set up this shit")
 # i also added this massgae just to confirm that i have learned that i can use github by vs code
+print("and after all this shits that happened i will come across this phenomena")
